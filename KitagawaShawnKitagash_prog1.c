d8bd42e5294ccbad25160cf981f616bf6087a65d
shawnkitagawa <112601888+shawnkitagawa@users.noreply.github.com>
2025-01-16 07:11:51 +0000

    update c file

--- KitagawaShawnKitagash_prog1.c
+++ KitagawaShawnKitagash_prog1.c
@@ -3,7 +3,7 @@
 
 double solve_a (double radius, double height_a)
 {
-    radius = radius * radius
+    radius = radius * radius;
     height_a = height_a * height_a;
 
     return sqrt(radius - height_a);
@@ -11,7 +11,7 @@ double solve_a (double radius, double height_a)
 
 double solve_b (double radius,double  height_b)
 {
-    radius = radius * radius
+    radius = radius * radius;
     height_b = height_b * height_b;
 
     return sqrt(radius - height_b);
@@ -24,13 +24,13 @@ double solve_height (double height_a, double height_b)
 
 double top_surface_area (double pi,double  a)
 {
-    a = a * a 
+    a = a * a; 
     return pi * a ;
 }
 
 double bottom_surface_area (double pi, double b)
 {
-    b = b * b
+    b = b * b;
     return pi * b;
 }
 
@@ -58,15 +58,69 @@ double volume (double pi, double radius, double height,double  a ,double b)
 }
 
 
+
 int main ()
 {
     int number = 0;
-    while (number < 0 || number > 10)
+    int start = 1;
+    while (number < 2 || number > 10)
     {
-        printf("How many spherical segments you want to evaluate");
+        printf("How many spherical segments you want to evaluate [2-10-]\n ");
         scanf("%d", &number);
 
     }
+    printf("done");
+
+    double radius = 0;
+    double height_a = 0;
+    double height_b = 0;
+    while (start <= number)
+    {
+        printf("Obtaining date for spherical segment number %d \n", start);
+        printf("What is the radius of the sphere (R)\n");
+        scanf("%lf", &radius);
+        printf("What is the height of the top area fo the spherical segment (ha)\n");
+        scanf("%lf", &height_a);
+        printf("What is the height of the bottom area of the spherical segment (hb)\n");
+        scanf("%lf", &height_b);
+        printf("Entered data: R = %lf  ha = %lf  hb = %lf  \n", radius, height_a, height_b);
+
+        if (height_a < 0)
+        {
+            printf("Invalid Input: height_a needs to be a positive integer\n");
+        }
+        else if (height_b < 0)
+        {
+            printf("Invalid Input: height_b needs to be a positive integer\n");
+        }
+        else if (radius < 0)
+        {
+            printf("Invalid Input: radius needs to be a positive integer\n");
+        }
+        else if (radius < height_a)
+        {
+            printf("Invalid Input: height_a cannot be beggier than the radius\n");
+        }
+        else if (radius < height_b)
+        {
+            printf("Invalid Input: height_b cannot be beggier than the radius\n");
+        }
+        else if (height_b > height_a)
+        {
+            printf("Invalid Input: height_b cannot be beggier than height_a\n");
+        }
+        else
+        {
+            start +=1;
+
+        }
+
+
+
+
+    }
+
+    
 
 
 
